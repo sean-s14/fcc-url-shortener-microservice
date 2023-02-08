a987acbece336491fb92f9d0bc710b6710d38139
Sean Stocker <sean.stocker14@gmail.com>
2023-02-08 23:34:53 +0000

    Altered index.js

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+const short_urls = {};
+
+app.post("/api/shorturl", (req, res) => {
+  const { url } = req.body;
+
+  const regex = new RegExp(/https?:\/\/.+\..+/);
+  if (!regex.test(url)) {
+    return res.json({ error: "invalid url" });
+  }
+
+  const id = Object.keys(short_urls).length + 1;
+  short_urls[id] = url;
+  console.log("Short Urls :", short_urls);
+
+  return res.json({ original_url: url, short_url: id });
+});
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const { shorturl } = req.params;
+  return res.redirect(short_urls[shorturl]);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
